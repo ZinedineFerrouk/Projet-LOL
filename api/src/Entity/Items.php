ea3afd41e1ff29544725807e83c94c28260a0a00
ZinedineFerrouk <zinedine27000@gmail.com>
2022-09-26 16:33:13 +0200

    Add: Items entity + ApiService

---
+++ api/src/Entity/Items.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ItemsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ItemsRepository::class)]
+class Items
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private array $info = [];
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $plainText = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $icon = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getInfo(): array
+    {
+        return $this->info;
+    }
+
+    public function setInfo(?array $info): self
+    {
+        $this->info = $info;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getPlainText(): ?string
+    {
+        return $this->plainText;
+    }
+
+    public function setPlainText(string $plainText): self
+    {
+        $this->plainText = $plainText;
+
+        return $this;
+    }
+
+    public function getIcon(): ?string
+    {
+        return $this->icon;
+    }
+
+    public function setIcon(string $icon): self
+    {
+        $this->icon = $icon;
+
+        return $this;
+    }
+}
