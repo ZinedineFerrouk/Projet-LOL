b877d5d90493aa0ae24b00802756f535eb30f99b
SkyyInfinity <dylan.hautecoeur@gmail.com>
2022-09-28 12:02:14 +0200

    home page completed (need to link to api)

--- front/src/components/searchbar/SearchBar.jsx
+++ front/src/components/searchbar/SearchBar.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './SearchBar.scss';
 import searchLine from '../../assets/img/icons/search-line.svg';
-import chevronDownLine from '../../assets/img/icons/chevron-down-line.svg';
 import { Link } from 'react-router-dom';
 
 const SearchBar = (props) => {
 
+    const [hideResults, setHideResults] = useState(true);
+    const [inputText, setInputText] = useState('');
+    const [playerIsEmpty, setPlayerIsEmpty] = useState(false);
     const PLAYERS = [
         {
             id: 1,
@@ -27,30 +29,79 @@ const SearchBar = (props) => {
             iconId: 4587,
             level: 256,
             region: 'EUW'
+        },
+        {
+            id: 3,
+            name: 'Skyrreez',
+            iconId: 4587,
+            level: 256,
+            region: 'EUW'
+        },
+        {
+            id: 3,
+            name: 'Skyrreez',
+            iconId: 4587,
+            level: 256,
+            region: 'EUW'
+        },
+        {
+            id: 3,
+            name: 'Skyrreez',
+            iconId: 4587,
+            level: 256,
+            region: 'EUW'
         }
-    ]
+    ];
+    
+    useEffect(() => {
+        if(PLAYERS.length === 0) {
+            setPlayerIsEmpty(true);
+        }
+    });
+
+    const triggerInput = (e) => {
+        if(e.target.value.length >= 3) {
+            // Get input value and show results
+            setInputText(e.target.value);
+            setHideResults(false);
+            
+            // call api
+
+            // if no response
+        } else {
+            setHideResults(true);
+        }
+    }
 
   return (
     <div className="searchbar">
         <div className="region select-container">
             <select className="input select" name="region" id="region">
-                <option value="">-- Région --</option>
+                <option value="">Choix de la région</option>
                 { 
                     props.regions.map((region, index) => {
                         return <option key={ index } value={ region.value }>{ region.name }</option>
                     })
                 }
             </select>
-            <img className="icon" src={ chevronDownLine } alt="chevron icon" />
         </div>
         <div className="input-container">
-            <input id="search" className="input input-no-radius" placeholder="Qui cherchez-vous ?" type="search" name="search" />
+            <input id="search" className="input input-no-radius" placeholder="Qui cherchez-vous ?" type="search" name="search" onInput={ triggerInput } />
         </div>
-        <ul className="results">
+        {playerIsEmpty ? (
+            <ul className="results" aria-hidden={ hideResults }>
+                <li className="result">
+                    <div className="is-empty">
+                        <span className="notice notice-info">Aucun joueur trouvé pour "{ inputText }".</span>
+                    </div>
+                </li>
+            </ul>
+        ) : (
+            <ul className="results" aria-hidden={ hideResults }>
             {
                 PLAYERS.map((player, index) => {
-                    return <Link to="/">
-                        <li key={ index } className="result">
+                    return <Link to="/" key={ index }>
+                        <li className="result">
                             <div className="player-icon">
                                 <img src={ `https://opgg-static.akamaized.net/images/profile_icons/profileIcon${player.iconId}.jpg` } alt="player icon" />
                             </div>
@@ -64,7 +115,9 @@ const SearchBar = (props) => {
                     </Link>
                 })
             }
-        </ul>
+            </ul>
+        )}
+        
         <button title="Rechercher" className="btn btn-icon-only"><img className="icon" src={ searchLine } alt="search icon" /></button>
     </div>
   )
