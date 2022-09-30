8b303e988741e249a895125ac9f1831ce1d8eae9
Zinedine <zinedine27000@gmail.com>
2022-09-30 17:05:23 +0200

    Add: Map + kills points

--- front/src/pages/map/MatchTimeline.jsx
+++ front/src/pages/map/MatchTimeline.jsx
@@ -1,23 +1,43 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { getMatchTimeline } from "../../Service/MatchService";
 import "./MatchTimeline.scss";
+import match_map from "../../assets/img/match-map.webp";
 
 const MatchTimeline = () => {
-  const [matchTimeline, setMatchTimeline] = useState({});
+  const [matchTimeline, setMatchTimeline] = useState({}); 
+  const [killInfos, setkillInfos] = useState([]);
+  const params = useParams();
 
   useEffect(() => {
+    let killInfosFormat = [];
     // Appeler la route API pour récupèrer les champions et le nom des invocateurs
-    let formatMatchTimeline = [];
 
-    getMatchTimeline("EUW1_6081980145").then((res) => {
-      for (let i = 0; i < res["data"][0]["frames"].length; i++) {
-        formatMatchTimeline.push(res["data"][0]["frames"][i]);
+    getMatchTimeline(params.match_id).then((res) => { 
+      setMatchTimeline(res["data"][0]["frames"]);
+
+      for (let i = 0; i < matchTimeline.length; i++) {
+        for (let j = 0; j < matchTimeline[i].events.length; j++) { 
+          if (matchTimeline[i].events[j].type === "CHAMPION_KILL") {     
+            killInfosFormat.push({
+              type: matchTimeline[i].events[j].type,
+              timestamp: matchTimeline[i].events[j].timestamp,
+              killerId: matchTimeline[i].events[j].killerId,
+              victimId: matchTimeline[i].events[j].victimId,
+              position: matchTimeline[i].events[j].position
+            });
+          }
+        }
       }
-      setMatchTimeline(formatMatchTimeline);
+
+      if (killInfos.length === 0) {
+        setkillInfos(killInfosFormat); 
+      }
     });
   }, []);
-  console.log(matchTimeline);
+
+  console.log(killInfos);
 
   return (
     <div className="page page-timeline">
@@ -26,7 +46,20 @@ const MatchTimeline = () => {
 
         {/* TIMELINE MAP */}
         <div className="map">
-          {/* <img src="../../assets/img/lol_map.webp" alt="" style={'width: 150px'}/> */}
+          {killInfos.map((info, index) => {
+            return (
+              <div
+                key={index}
+                className="point"
+                style={{
+                  left: (info.position.x * 100) / 15000 + "%", 
+                  bottom: (info.position.y * 100) / 15000 + "%",
+                }}
+              ></div>
+            );
+          })}
+
+          <img className="lol_map" src={match_map} />
         </div>
         <div className="red-team"></div>
       </div>
