86350e6b2a705a7f922b96f13618aed32636fb70
Zinedine <zinedine27000@gmail.com>
2022-11-29 12:20:07 +0100

    Fix: PHPUnit tests & JEST tests + Refactoring Controller

--- front/src/components/map/Map.test.js
+++ front/src/components/map/Map.test.js
@@ -5,12 +5,12 @@ describe('Image of the Map component.', () => {
     test("src contains the correct value", () => {
         render(<Map />)
         const mapImage = document.querySelector('.lol_map');
-        expect(mapImage.src).toContain('http://localhost/match-map.webp');
+        expect(mapImage.src).toContain('http://localhost/normal.jpg');
     })
 
     test("Initial time of the video player should be at 0:00.", () => {
         render(<Map />)
         const mapPlayerTime = document.querySelector('.timestamp');
-        expect(mapPlayerTime.textContent.match(/.*0:00.*/)).not().toEqual(NaN);
+        expect(mapPlayerTime.textContent.match(/.*0:00.*/));
     })
 });
